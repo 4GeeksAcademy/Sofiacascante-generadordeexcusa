71053a148b27d76b2bf925188b5e24ea93b7093d
sofi1967 <132711967+sofi1967@users.noreply.github.com>
2023-07-04 20:52:29 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  document.querySelector("#generador").innerHTML = excusa();
   console.log("Hello Rigo from the console!");
+};
+let excusa = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let who1 = Math.floor(Math.random() * who.length);
+  let action1 = Math.floor(Math.random() * action.length);
+  let what1 = Math.floor(Math.random() * what.length);
+  let when1 = Math.floor(Math.random() * when.length);
+  return who[who1] + action[action1] + what[what1] + when[when1];
 };
